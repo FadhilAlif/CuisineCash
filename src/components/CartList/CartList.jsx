e6346f81f41495f6f48a9597d04850d9b5c4215a
FadhilAlif <92931140+FadhilAlif@users.noreply.github.com>
2023-10-18 13:56:51 +0700

    add: CartList component

---
+++ src/components/CartList/CartList.jsx
@@ -0,0 +1,14 @@
+import { Col } from "react-bootstrap";
+
+const CartList = () => {
+  return (
+    <Col md={3}>
+      <h4>
+        <strong>Cart</strong>
+      </h4>
+      <hr></hr>
+    </Col>
+  );
+};
+
+export default CartList;
