c81e9f23dcbd650964cd2d0c63c59e58aa30fbb6
FadhilAlif <92931140+FadhilAlif@users.noreply.github.com>
2023-10-18 13:54:25 +0700

    add some minor update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    // env from .env file
+    "process.env.FIREBASE_API_KEY": JSON.stringify(
+      process.env.FIREBASE_API_KEY
+    ),
+  },
+});
